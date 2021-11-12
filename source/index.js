b51468bea5d8c681ac0324bac6cda64b2c544841
BuildTools <lily@l1lith.com>
2021-11-11 22:00:03 -0700

    get the project to not be dumb maybe

--- source/index.js
+++ source/index.js
@@ -1,3 +1,4 @@
+import useRapier from './useRapier'
 // Ensure we have one of the rapier libs
 try {
   require('@dimforge/rapier3d')
@@ -8,3 +9,5 @@ try {
     throw new Error('Please install either @dimforge/rapier2d or @dimforge/rapier3d')
   }
 }
+
+export { useRapier }
